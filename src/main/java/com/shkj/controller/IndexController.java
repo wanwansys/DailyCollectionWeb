6492e5ecd877901851b0d2767b9dcf07b3f26cef
wanwansys <1581153295@qq.com>
2020-09-19 17:20:17 +0800

    验签取值

--- src/main/java/com/shkj/controller/IndexController.java
+++ src/main/java/com/shkj/controller/IndexController.java
@@ -3,15 +3,17 @@ package com.shkj.controller;
 
 import java.io.UnsupportedEncodingException;
 
+
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
 import com.cmbchina.channel.SMCryptException;
 import com.cmbchina.channel.api.SM2;
 import com.shkj.bean.WhiteUser;
@@ -31,14 +33,14 @@ public class IndexController {
 	@RequestMapping("/VerifyData")
 	@ResponseBody
 	public String VerifyData(String data,String token){			
-		try {//
+		try {
 			//String data = "eyJ5c3RJRCI6IuS4gOS6i+mAmuWPtyIsIm9yZ0lEIjoi5py65p6E57yW5Y+3IiwidGltZSI6IjIwMTUtMTItMjhUMTY6NTk6NDEiLCJpcCI6IjE5Mi4xNjguMTE1LjMyIiwic2FwSUQiOiLlkZjlt6Xlj7ciLCJlbWFpbCI6Im9tYXNfdGVzdF8wMUBjbWJjaGluYS5jb20iLCJuYW1lIjoi5aeT5ZCNIiwiYnIxIjoiMTAwMDAzIiwicGF0aE5hbWUiOiLmnLrmnoTot6/lvoTvvJov5oC76KGML1hYWOmDqC9YWFjlrqQiLCJncElEIjoiMDEwMDA0NThRMCIsInN5c0lEIjoi57O757ufaWQiLCJvc1ZlcnNpb24iOiI5LjAuMiIsInB1YlZlcnNpb24iOiIxMjI0MDEiLCJwbGF0Zm9ybSI6InBjIiwicGF0aElEIjoiLzEwMDAwMy8wMDAwMDAiLCJkZXZpY2VJRCI6IiIsIm9yZ05hbWUiOiLmnLrmnoTlkI3np7AiLCJnZW5kZXIiOiLmgKfliKsifQ==";
 			//String token = "de42b1b6dc58b5fe49fc536a99fca5c15d054852b264ad9b8eba37a4b9fa9876c520351d383ede13f16f7de49ae4c9566946edff3967a93312aa2f579e9d97cb";
 			//验证集成登录信息合法性，获取用户id（公钥验证）
 			byte[] s = Base64.getDecoder().decode(data);
 			String plainTextString = new String(s ,"utf-8");
 			//将json字符串转化为JSONObject 
-			JSONObject jsStr = new JSONObject().getJSONObject(plainTextString); 
+			JSONObject jsStr = JSONObject.parseObject(plainTextString);
 			//通过getString("")分别取出里面的信息(data);			 
              //公钥字符串
              byte[] publicBytes = Base64.getDecoder().decode(pubKeyParam);
@@ -51,7 +53,7 @@ public class IndexController {
 	       	     String ystID = jsStr.getString("ystID"); 
 	       	     WhiteUser whiteUser = whiteUserService.getWhiteUser(ystID);
 	       	     if(null != whiteUser) {
-	       	    	 return "{\"result\":\"success\",\"msg\":\"验证通过成功\"}";
+	       	    	 return "{\"result\":\"success\",\"msg\":\"" + JSON.toJSON(whiteUser) + "\"}";
 	       	     }else {
 	       	    	 return "{\"result\":\"fail\",\"msg\":\"没有权限访问\"}";
 	       	     }
